1e68373a4582c3474290b75765233fb59af2065f
Jaden Accord <jbr.accord@gmail.com>
2023-09-18 15:23:33 +0200

    Send requests to API

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,48 @@ use std::{
         self,
         BufRead,
         BufReader,
+        Read,
     },
     path::Path,
 };
-
 use dirs;
+use reqwest::{
+    self,
+    header::*,
+};
+use serde_json::json;
 
 const REL_CONFIG_PATH: &str = ".config/chatgptr/chatgptr.conf";
 
-fn main() -> Result<(), io::Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::load()?;
     println!("Token: {}", config.token);
+
+    // let mut message: String = String::new();
+    // println!("\n Message:");
+    // io::stdin().read_line(&mut message).expect("failed to readline");
+
+    let payload = json!({
+        "model": "gpt-3.5-turbo",
+        "messages": [{
+            "role": "user",
+            "content": "What is Rustlang, in 1 sentence.",
+        }],
+        "temperature" : 0.7,
+    });
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post("https://api.openai.com/v1/chat/completions")
+        .header(CONTENT_TYPE, "application/json")
+        .header(AUTHORIZATION, format!("Bearer {}", config.token))
+        .body(payload.to_string())
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{:?}", response);
 
     Ok(())
 }
@@ -30,31 +61,54 @@ struct Config {
 }
 
 impl Config {
+    fn new() -> Config {
+        Config {
+            token: String::new(),
+            model: GPTModel::V3_5,
+        }
+    }
+
     fn load() -> Result<Config, io::Error> {
         let path = dirs::home_dir().unwrap().join(Path::new(REL_CONFIG_PATH));
 
         if let Ok(file) = File::open(path) {
+            let mut config = Config::new();
+
+            // let token_reader = BufReader::new(&file);
+
+            // let test_token = token_reader
+            //     .lines()
+            //     .find(|line| {
+            //         line
+            //             .as_ref()
+            //             .unwrap()
+            //             .starts_with("token=")
+            //     })
+            //     .unwrap()?
+            //     .split("=")
+            //     .map(String::from)
+            //     .skip(1)
+            //     .next()
+            //     .unwrap();
+
             let reader = BufReader::new(file);
-
-            let token = reader
+            reader
                 .lines()
-                .find(|line| {
-                    line
-                        .as_ref()
-                        .unwrap()
-                        .starts_with("token=")
-                })
-                .unwrap()?
-                .split("=")
-                .map(String::from)
-                .skip(1)
-                .next()
-                .unwrap();
-
-            let config = Config {
-                token,
-                model: GPTModel::V4,
-            };
+                .filter_map(|line| line.ok())
+                .filter(|line| line.contains("="))
+                .for_each(|line| {
+                    let key = line.split("=").next().unwrap();
+                    let value = line.split("=").skip(1).next().map(String::from).unwrap();
+                    match key {
+                        "token" => config.token = value,
+                        "model" => config.model = match value.as_str() {
+                            "gpt-3.5-turbo" => GPTModel::V3_5,
+                            "gpt-4" => GPTModel::V4,
+                            _ => GPTModel::V3_5,
+                        },
+                        _ => {}
+                    }
+                });
 
             Ok(config)
         } else {
