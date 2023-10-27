5fb159c81192e2b167c4e285560196039c69f2b2
Jaden Accord <jbr.accord@gmail.com>
2023-10-27 17:53:23 +0200

    feat: parse Config from config.toml file

--- src/io/config.rs
+++ src/io/config.rs
@@ -1,6 +1,9 @@
-use toml::Table;
+use serde::Deserialize;
 
-#[derive(Clone, Default)]
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Clone, Default, Deserialize)]
 pub struct Config {
     api_key: Option<String>,
 }
@@ -12,9 +15,16 @@ impl Config {
         }
     }
 
-    // pub fn load() -> Self {
-    // }
+    pub fn load() -> Self {
+        let xdg_dirs = xdg::BaseDirectories::with_prefix("chatgptr").unwrap();
+        // let path = xdg_dirs.find_config_file("config.toml").unwrap_or_else(|| xdg_dirs.place_config_file("config.toml").expect("could not create config file"));
+        let path = xdg_dirs.find_config_file("config.toml").expect("config.toml does not exist");
+        let mut file = File::open(path).expect("could not open config.toml");   // TODO: unwrap_or_else() instead of unwrap()
+        let mut content = String::new();
+        file.read_to_string(&mut content).expect("could not read config.toml");
 
-    // pub fn from_str(text: &str) -> Self {
-    // }
+        toml::from_str(&content).unwrap()
+    }
+
+    pub fn api_key(&self) -> Option<String> { self.api_key.clone() }
 }
