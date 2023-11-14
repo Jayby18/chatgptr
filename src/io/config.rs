af43089b9aca0954e193c6ec6951ca17d9871e2f
Jaden Accord <jbr.accord@gmail.com>
2023-11-14 14:46:10 +0100

    feat: ask ChatGPT questions in simple CLI

--- src/io/config.rs
+++ src/io/config.rs
@@ -3,15 +3,17 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
 
-#[derive(Clone, Default, Deserialize)]
+#[derive(Clone, Deserialize, Debug)]
 pub struct Config {
     api_key: Option<String>,
+    model: Option<String>,
 }
 
 impl Config {
-    pub fn new(api_key: String) -> Self {
+    pub fn new(api_key: String, model: String) -> Self {
         Config {
             api_key: Some(api_key),
+            model: Some(model),
         }
     }
 
@@ -26,4 +28,14 @@ impl Config {
     }
 
     pub fn api_key(&self) -> Option<String> { self.api_key.clone() }
+    pub fn model(&self) -> Option<String> { self.model.clone() }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            api_key: None,
+            model: None,
+        }
+    }
 }
